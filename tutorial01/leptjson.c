48333c91ea3f5acbf4f53a5844842281fcf7ece6
laoqu <quguiyou1076@qq.com>
2022-11-06 19:18:25 +0800

    数组解析

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -207,6 +207,56 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
 	}
 }
 
+static int lept_parse_value(lept_context* c, lept_value* v);
+
+static int lept_parse_array(lept_context* c, lept_value* v) {
+	size_t size = 0;
+	int ret;
+	EXPECT(c, '[');
+	lept_parse_whitespace(c);
+	if (*c->json == ']') {
+		c->json++;
+		v->type = LEPT_ARRAY;
+		v->u.a.size = 0;
+		v->u.a.e = NULL;
+		return LEPT_PARSE_OK;
+	}
+	for (;;) {
+		lept_value e;
+		lept_init(&e);
+		if ((ret = lept_parse_value(c, &e)) != LEPT_PARSE_OK) {
+			break;
+		}
+		memcpy(lept_context_push(c, sizeof(lept_value)), &e, sizeof(lept_value));
+		size++;
+		lept_parse_whitespace(c);
+		if (*c->json == ',') {
+			c->json++;
+			lept_parse_whitespace(c);
+		}
+		else if (*c->json == ']') {
+			c->json++;
+			v->type = LEPT_ARRAY;
+			v->u.a.size = size;
+			size *= sizeof(lept_value);
+			memcpy(v->u.a.e = (lept_value*)malloc(size), lept_context_pop(c, size), size);
+			return LEPT_PARSE_OK;
+		}
+		else {
+			ret = LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+			break;
+		}
+			
+	}
+
+	/* Pop and free values on the stack */
+	for (int i = 0; i < size; ++i) {
+		lept_free((lept_value*)lept_context_pop(c, sizeof(lept_value)));
+	}
+
+	return ret;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
 	switch (*c->json)
 	{
@@ -214,6 +264,7 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 	case 't': return lept_parse_literal(c, v, "true", LEPT_TRUE);
 	case 'f': return lept_parse_literal(c, v, "false", LEPT_FALSE);
 	case '"': return lept_parse_string(c, v);
+	case '[': return lept_parse_array(c, v);
 	case '\0': return LEPT_PARSE_EXPECT_VALUE;
 	default: return lept_parse_number(c, v);
 	}
@@ -278,9 +329,23 @@ void lept_set_boolean(lept_value* v, int b) {
 
 void lept_free(lept_value* v) {
 	assert(v != NULL);
-	if (v->type == LEPT_STRING) {
+
+	size_t i;
+	switch (v->type)
+	{
+	case LEPT_STRING:
 		free(v->u.s.s);
+		break;
+	case LEPT_ARRAY:
+		for (i = 0; i < v->u.a.size; i++) {
+			lept_free(&v->u.a.e[i]);
+		}
+		free(v->u.a.e);
+		break;
+	default:
+		break;
 	}
+
 	v->type = LEPT_NULL;
 }
 
@@ -292,4 +357,14 @@ void lept_set_string(lept_value* v, const char* s, size_t len) {
 	v->u.s.s[len] = '\0';
 	v->u.s.len = len;
 	v->type = LEPT_STRING;
+}
+
+size_t lept_get_array_size(const lept_value* v) {
+	assert(v != NULL && v->type == LEPT_ARRAY);
+	return v->u.a.size;
+}
+lept_value* lept_get_array_element(const lept_value* v, size_t index) {
+	assert(v != NULL && v->type == LEPT_ARRAY);
+	assert(index < v->u.a.size);
+	return &v->u.a.e[index];
 }
